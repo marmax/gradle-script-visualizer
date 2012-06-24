24e4c51ff0de8434596141e28396e9610e881941
douglas.bullard <douglas.bullard@gmail.com>
2012-06-24 22:09:10 +0000

    Cleaned up output code a bit

--- src/main/java/com/nurflugel/util/Util.java
+++ src/main/java/com/nurflugel/util/Util.java
@@ -23,7 +23,7 @@ public class Util
 {
   public static final Cursor busyCursor   = Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR);
   public static final Cursor normalCursor = Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR);
-  public static final String VERSION      = "1.0.3";
+  public static final String VERSION      = "1.0.4";
   // -------------------------- STATIC METHODS --------------------------
 
   /** Sets the look and feel. */

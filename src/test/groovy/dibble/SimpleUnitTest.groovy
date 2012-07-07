eed9873c2c13a3ea5f44fd7e900fb8f98390cdc9
douglas.bullard <douglas.bullard@gmail.com>
2012-07-07 06:26:06 +0000

    Testing JDK 1.7 compiled to jdk 1.6

--- src/test/groovy/dibble/SimpleUnitTest.groovy
+++ src/test/groovy/dibble/SimpleUnitTest.groovy
@@ -1,11 +1,16 @@
+import com.nurflugel.util.gradlescriptvisualizer.parser.GradleFileParser
+import com.nurflugel.util.gradlescriptvisualizer.domain.Task
 
+class MyTest extends GroovyTestCase
+ {
 
-
-class SimpleUnitTest extends GroovyTestCase {
-  void testSimple(){
-    assertEquals("Should add ",2,1+1)
-    
-    
-  }
-  
+//   void dibble(){
+//     Map<String, Task> taskMap
+//     askap.dirgel()
+//     GradleFileParser.addToTaskMap(taskMap,null,null)
+//   }
+    void testSomething() {
+        assert 1 == 1
+        assert 2 + 2 == 5 : "We're in trouble, arithmetic is broken"
+    }
 }

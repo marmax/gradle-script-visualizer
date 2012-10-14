17728ee528001a48740f2f5fe24fc8896c283f78
douglas.bullard@gmail.com <douglas.bullard@gmail.com@7e61aac2-d88c-517a-232f-853ffb600877>
2012-10-14 03:07:14 +0000

    JavaFX conversion

--- trunk/src/main/java/com/nurflugel/gradle/ui/Main.java
+++ trunk/src/main/java/com/nurflugel/gradle/ui/Main.java
@@ -7,6 +7,7 @@ import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import java.net.URL;
+import static com.nurflugel.util.Util.VERSION;
 import static com.nurflugel.util.gradlescriptvisualizer.domain.Os.findOs;
 import static java.util.logging.Level.SEVERE;
 import static java.util.logging.Logger.getLogger;
@@ -14,6 +15,8 @@ import static javafx.fxml.FXMLLoader.load;
 
 public class Main extends Application
 {
+  public static final String TITLE_TEXT = "Gradle Script Visualizer v";
+
   /** @param  args  the command line arguments */
   public static void main(String... args)
   {
@@ -31,7 +34,7 @@ public class Main extends Application
       Scene      scene    = new Scene(page);
 
       primaryStage.setScene(scene);
-      primaryStage.setTitle("Gradle Visualizer");
+      primaryStage.setTitle(TITLE_TEXT + VERSION);
       primaryStage.show();
     }
     catch (Exception ex)

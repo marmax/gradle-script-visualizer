ce2bcb090d856dba6c02017d7fc4187dc026bc9a
douglas.bullard <douglas.bullard@gmail.com>
2012-11-05 02:45:02 +0000

    Fixed bug in unit test

--- src/test/java/com/nurflugel/util/dependencyvisualizer/output/DependencyDotFileGeneratorTest.java
+++ src/test/java/com/nurflugel/util/dependencyvisualizer/output/DependencyDotFileGeneratorTest.java
@@ -1,16 +1,25 @@
 package com.nurflugel.util.dependencyvisualizer.output;
 
+import com.nurflugel.gradle.ui.dialog.ConfigurationChoiceDialog;
+import com.nurflugel.gradle.ui.dialog.ConfigurationsDialogBuilder;
+
 import com.nurflugel.util.dependencyvisualizer.parser.GradleDependencyParser;
 import com.nurflugel.util.gradlescriptvisualizer.domain.Os;
 import com.nurflugel.util.gradlescriptvisualizer.ui.GradleScriptPreferences;
+import static com.nurflugel.util.test.TestResources.getFilePath;
+
+import static org.apache.commons.io.FileUtils.readLines;
+
 import org.testng.Assert;
+
+import static org.testng.Assert.fail;
+
 import org.testng.annotations.Test;
+
 import java.io.File;
 import java.io.IOException;
+
 import java.util.List;
-import static com.nurflugel.util.test.TestResources.getFilePath;
-import static org.apache.commons.io.FileUtils.readLines;
-import static org.testng.Assert.fail;
 
 /** Created with IntelliJ IDEA. User: douglas_bullard Date: 10/3/12 Time: 13:03 To change this template use File | Settings | File Templates. */
 @Test(groups = "unit")
@@ -24,15 +33,17 @@ public class DependencyDotFileGeneratorTest
 
   public void testReadFile1() throws IOException, NoConfigurationsFoundException
   {
-    String                     filePath       = getFilePath("gradle/dependencies/Das_dependencies.txt");
-    List<String>               lines          = readLines(new File(filePath));
-    GradleDependencyParser     parser         = new GradleDependencyParser();
-    GradleScriptPreferences    preferences    = new GradleScriptPreferences();
+    String                  filePath    = getFilePath("gradle/dependencies/Das_dependencies.txt");
+    List<String>            lines       = readLines(new File(filePath));
+    GradleDependencyParser  parser      = new GradleDependencyParser();
+    GradleScriptPreferences preferences = new GradleScriptPreferences();
+
+    preferences.setShouldJustUseCompileConfig(true);
+
     String                     outputFileName = "das_dibble.dot";
     DependencyDotFileGenerator generator      = new MockDependencyDotFileGenerator();
 
-    // generator.createDotFileFromLines(parser, preferences, outputFileName, lines.toArray(new String[lines.size()]), Os.findOs(), dialog);
-    fail("Uncomment and fix");
+    generator.createDotFileFromLines(parser, preferences, outputFileName, lines.toArray(new String[lines.size()]), Os.findOs(), null);
   }
 
   @Test(groups = "long")
